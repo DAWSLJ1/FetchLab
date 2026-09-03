24edef5b870f06cf331932645823fa4cf9bbac14
DAWSLJ1 <DAWSLJ1@student.op.ac.nz>
2026-09-04 11:04:28 +1200

    limit script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const requestURL = "https://pokeapi.co/api/v2/pokemon/tapu-koko";
+let limit = 10;
+let offset = 0;
+const requestURL = "https://pokeapi.co/api/v2/pokemon?limit${limit}&offset${offset}";
 async function getData() {
 const res = await fetch(url);
 console.log(await res);
